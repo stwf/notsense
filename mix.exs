e3a5d0f641c3821aed76abad74ccf8d007482959
Steven Fuchs <stwf@mac.com>
2016-07-21 14:04:28 -0400

    fix dips

--- mix.exs
+++ mix.exs
@@ -6,12 +6,12 @@ defmodule Notsense.Mixfile do
      version: "0.2.2",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
-     deps: deps,
+     deps: deps(),
      name: "Notsense",
      source_url: "https://github.com/tehsnappy/notsense",
      compilers: [:phoenix] ++ Mix.compilers,
-     description: description,
-     package: package]
+     description: description(),
+     package: package()]
   end
 
   # Configuration for the OTP application
@@ -20,10 +20,7 @@ defmodule Notsense.Mixfile do
   end
 
   defp deps do
-    [
-      {:phoenix, "~> 1.2.0-rc", only: :dev},
-      {:phoenix_html, "~> 2.5", only: :dev}
-    ]
+    []
   end
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
 
