be001bb8620d0317765f99fbb80b7e4dc179810f
Steven Fuchs <stwf@mac.com>
2016-08-30 23:40:37 -0400

    work?

--- web/web.ex
+++ web/web.ex
@@ -5,15 +5,8 @@ defmodule Notsense.Web do
     quote do
       require Logger
 
-
-      file_path = __ENV__.file
-      |> Path.dirname
-      |> String.split("/views")
-      |> hd
-      |> Path.join("templates")
-
-      use Phoenix.View, root: file_path
-      # use Phoenix.View, root: "web/templates"
+      # use Phoenix.View, root: file_path
+      use Phoenix.View, root: "web/templates"
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [view_module: 1]
