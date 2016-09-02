aeb48e41cbfdd3b59d92697cdda3b9a1a6924474
Steven Fuchs <stwf@mac.com>
2016-09-01 21:01:02 -0400

    for prod

--- mix.exs
+++ mix.exs
@@ -24,9 +24,9 @@ defmodule Notsense.Mixfile do
 
   defp deps do
     [
-      {:phoenix, "~> 1.2.0", only: :dev},
-      {:calliope, "~> 0.4.0" },
-      {:phoenix_html, "~> 2.5", only: :dev}
+      {:phoenix, "~> 1.2.0"},
+      {:phoenix_haml, "~> 0.2.1"},
+      {:phoenix_html, "~> 2.5"}
     ]
   end
 
